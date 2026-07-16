efbadcb5e69f5400315585d3e38726d427057d21
Sherif Samir <sherif.samir@gmail.com>
2026-07-16 16:29:28 +0400

    Rebuild homepage responsive layout

--- voylme-web/app/search/page.tsx
+++ voylme-web/app/search/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import styles from "./home-clean.module.css";
+import styles from "./home-page.module.css";
 
 import AIHelpButton from "../../components/AIHelpButton";
 import Footer from "../../components/Footer";
@@ -11,22 +11,29 @@ import SearchForm from "../../components/search/SearchForm";
 
 export default function SearchPage() {
   return (
-    <main className={`${styles.home} voylme-final-viewport`}>
-      <div className="voylme-final-page">
-        <AppHeader />
+    <main className={styles.page}>
+      <AppHeader />
+
+      <div className={styles.content}>
         <Hero />
-        <TravelServices />
+
+        <div className={styles.services}>
+          <TravelServices />
+        </div>
 
         <section
-          className="voylme-final-search"
+          className={styles.search}
           aria-label="Flight search"
         >
           <SearchForm />
         </section>
 
-        <Footer />
-        <AIHelpButton />
+        <div className={styles.footer}>
+          <Footer />
+        </div>
       </div>
+
+      <AIHelpButton />
     </main>
   );
 }
